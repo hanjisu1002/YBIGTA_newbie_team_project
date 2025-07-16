42ec3d6bab0afb6c1a746e28ca884b6ae84ca14f
hanjisu1002 <hanjisu1002@yonsei.ac.kr>
2025-07-16 19:11:09 +0900

    router docstring

--- YBIGTA_newbie_team_project/app/user/user_router.py
+++ YBIGTA_newbie_team_project/app/user/user_router.py
@@ -9,6 +9,14 @@ user = APIRouter(prefix="/api/user")
 
 @user.post("/login", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def login_user(user_login: UserLogin, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    """
+    사용자 로그인
+
+    - 입력: 이메일과 비밀번호 (`UserLogin`)
+    - 처리: 이메일로 유저 조회 후 비밀번호 일치 여부 확인
+    - 응답: 로그인 성공 시 유저 정보 반환
+    - 예외: 유저가 존재하지 않거나 비밀번호 불일치 시 400 에러
+    """
     try:
         user = service.login(user_login)
         return BaseResponse(status="success", data=user, message="Login Success.") 
@@ -18,6 +26,14 @@ def login_user(user_login: UserLogin, service: UserService = Depends(get_user_se
 
 @user.post("/register", response_model=BaseResponse[User], status_code=status.HTTP_201_CREATED)
 def register_user(user: User, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    """
+    사용자 회원가입
+
+    - 입력: 유저 정보 (`User`)
+    - 처리: 이메일 중복 확인 후 등록
+    - 응답: 등록 성공 시 유저 정보 반환
+    - 예외: 이메일 중복 시 400 에러
+    """
     ## TODO
     try:
         user = service.register_user(user)
@@ -30,6 +46,14 @@ def register_user(user: User, service: UserService = Depends(get_user_service)) 
 
 @user.delete("/delete", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    """
+    사용자 삭제
+
+    - 입력: 삭제할 유저의 이메일 (`UserDeleteRequest`)
+    - 처리: 해당 이메일로 유저 존재 여부 확인 후 삭제
+    - 응답: 삭제 성공 시 삭제된 유저 정보 반환
+    - 예외: 유저가 존재하지 않으면 404 에러
+    """
     ## TODO
     try:
         user = service.delete_user(user_delete_request.email)
@@ -42,6 +66,14 @@ def delete_user(user_delete_request: UserDeleteRequest, service: UserService = D
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    """
+    사용자 비밀번호 변경
+
+    - 입력: 이메일, 새 비밀번호 (`UserUpdate`)
+    - 처리: 이메일로 유저 조회 후 비밀번호 변경
+    - 응답: 변경된 유저 정보 반환
+    - 예외: 유저가 존재하지 않으면 404 에러
+    """
     ## TODO
     try:
         user = service.update_user_pwd(user_update)
@@ -49,4 +81,3 @@ def update_user_password(user_update: UserUpdate, service: UserService = Depends
     
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
-
