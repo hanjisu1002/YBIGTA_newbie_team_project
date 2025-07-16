d5d01c0b558ea43d6f14725270bab4b486436860
구남혁 <knh0815nick@yonsei.ac.kr>
2025-07-16 19:19:30 +0900

    user_service.py docstring

--- YBIGTA_newbie_team_project/app/user/user_service.py
+++ YBIGTA_newbie_team_project/app/user/user_service.py
@@ -8,6 +8,19 @@ class UserService:
     # 두 dictionary를 비교해서 응답생성, 로그인 여부 결정
     def login(self, user_login: UserLogin) -> User:
         ## TODO
+         """
+        사용자 로그인 정보를 받아 로그인 처리를 수행합니다.
+
+        Args:
+            user_login (UserLogin): 로그인할 사용자의 이메일과 비밀번호 정보
+
+        Returns:
+            User: 로그인에 성공한 사용자 정보
+
+        Raises:
+            ValueError: 이메일이 존재하지 않을 때
+            ValueError: 비밀번호가 틀렸을 때
+        """
         user = self.repo.get_user_by_email(user_login.email)
         if user is None:
             raise ValueError("User not Found.")
@@ -18,6 +31,18 @@ class UserService:
     # 새로운 유저 등록
     def register_user(self, new_user: User) -> User:
         ## TODO
+        """
+        새로운 사용자를 등록합니다.
+
+        Args:
+            new_user (User): 회원가입할 사용자의 이메일, 비밀번호, 이름 정보
+
+        Returns:
+            User: 회원가입에 성공한 사용자 정보
+
+        Raises:
+            ValueError: 이미 동일한 이메일이 존재할 때
+        """
         existing_user = self.repo.get_user_by_email(new_user.email)          
         if existing_user is not None:
             raise ValueError("User already Exists.")
@@ -27,7 +52,19 @@ class UserService:
     
     # 유저 삭제
     def delete_user(self, email: str) -> User:
-        ## TODO        
+        ## TODO    
+        """
+        이메일을 받아 해당 사용자를 삭제(탈퇴)합니다.
+
+        Args:
+            email (str): 탈퇴할 사용자의 이메일
+
+        Returns:
+            User: 삭제된 사용자 정보
+
+        Raises:
+            ValueError: 이메일이 존재하지 않을 때
+        """    
         deleted_user = self.repo.get_user_by_email(email)          
         if deleted_user is None:
             raise ValueError("User not Found.")
@@ -37,6 +74,18 @@ class UserService:
     # 유저 정보 변경
     def update_user_pwd(self, user_update: UserUpdate) -> User:
         ## TODO
+        """
+        사용자의 비밀번호를 새 값으로 변경합니다.
+
+        Args:
+            user_update (UserUpdate): 이메일과 새 비밀번호 정보
+
+        Returns:
+            User: 비밀번호 변경이 완료된 사용자 정보
+
+        Raises:
+            ValueError: 이메일이 존재하지 않을 때
+        """
         updated_user = self.repo.get_user_by_email(user_update.email)
         if updated_user is None:
             raise ValueError("User not Found")
