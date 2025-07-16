4d584cdcefabfafcfbbff1e9a549d26ed766a7cf
hanjisu1002 <hanjisu1002@yonsei.ac.kr>
2025-07-16 18:53:13 +0900

    feature implementation complete

--- YBIGTA_newbie_team_project/app/user/user_router.py
+++ YBIGTA_newbie_team_project/app/user/user_router.py
@@ -32,7 +32,7 @@ def register_user(user: User, service: UserService = Depends(get_user_service)) 
 def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
     try:
-        user = service.delete_user(user_delete_request)
+        user = service.delete_user(user_delete_request.email)
         return BaseResponse(status="success", data=user, message="User Deletion Success.")
     
     except ValueError as e:
