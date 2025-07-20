9b241757f9d239082b62c32f1c812fc94a25453e
hanjisu1002 <hanjisu1002@yonsei.ac.kr>
2025-07-21 04:54:41 +0900

    detail correction and docstring

--- review_analysis/crawling/main.py
+++ review_analysis/crawling/main.py
@@ -2,14 +2,14 @@ from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.crawling.base_crawler import BaseCrawler
 from review_analysis.crawling.naver_crawler import NaverCrawler
-from review_analysis.crawling.emart_crawler import EmartCrawler
+#from review_analysis.crawling.emart_crawler import EmartCrawler
 from review_analysis.crawling.LotteOn_crawler import LotteOnCrawler
 
 
 # 모든 크롤링 클래스를 예시 형식으로 적어주세요. 
 CRAWLER_CLASSES: Dict[str, Type[BaseCrawler]] = {
     "naver": NaverCrawler,
-    "emart": EmartCrawler,
+    #"emart": EmartCrawler,
     "lotteon": LotteOnCrawler,
 }
 
