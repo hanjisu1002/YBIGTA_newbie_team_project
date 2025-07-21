d51ea842ead622bcda3c538ff7f3aa2abcf5cb5e
yeseo <0723abby@gmail.com>
2025-07-22 00:55:18 +0900

    Emart 크롤러 수정 반영

--- review_analysis/crawling/main.py
+++ review_analysis/crawling/main.py
@@ -1,13 +1,6 @@
 from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.crawling.base_crawler import BaseCrawler
-<<<<<<< HEAD
-from review_analysis.crawling.emart_crawler import EmartCrawler
-
-# 모든 크롤링 클래스를 예시 형식으로 적어주세요. 
-CRAWLER_CLASSES: Dict[str, Type[BaseCrawler]] = {
-    "emart": EmartCrawler,
-=======
 from review_analysis.crawling.naver_crawler import NaverCrawler
 from review_analysis.crawling.emart_crawler import EmartCrawler
 from review_analysis.crawling.LotteOn_crawler import LotteOnCrawler
@@ -17,10 +10,7 @@ from review_analysis.crawling.LotteOn_crawler import LotteOnCrawler
 CRAWLER_CLASSES: Dict[str, Type[BaseCrawler]] = {
     "naver": NaverCrawler,
     "emart": EmartCrawler,
-    "lotteon": LotteOnCrawler,
->>>>>>> main
 }
-
 
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser()
@@ -39,14 +29,12 @@ if __name__ == "__main__":
         for crawler_name in CRAWLER_CLASSES.keys():
             Crawler_class = CRAWLER_CLASSES[crawler_name]
             crawler = Crawler_class(args.output_dir)
-            crawler.start_browser()
             crawler.scrape_reviews()
             crawler.save_to_database()
      
     elif args.crawler:
         Crawler_class = CRAWLER_CLASSES[args.crawler]
         crawler = Crawler_class(args.output_dir)
-        crawler.start_browser()
         crawler.scrape_reviews()
         crawler.save_to_database()
     
