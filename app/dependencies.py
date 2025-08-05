d7dc6e78667458b01cd4bdb552ca502b808a6562
남구혁의 MacBook <namkuhyuk-uimacbook@namguhyeog-uiui-MacBookAir.local>
2025-08-05 18:07:30 +0900

    db

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,19 @@
 from fastapi import Depends
 from app.user.user_repository import UserRepository
 from app.user.user_service import UserService
+from sqlalchemy.orm import Session
+from database.mysql_connection import Base
 
-def get_user_repository() -> UserRepository:
-    return UserRepository()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_user_repository(db: Session = Depends(get_db)) -> UserRepository:
+    return UserRepository(db)
 
 def get_user_service(repo: UserRepository = Depends(get_user_repository)) -> UserService:
     return UserService(repo)
